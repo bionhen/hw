423fc6d20eec340cdf59ed916f14bda1e1e8b6a5
Smeshutka <90688272+Smeshutka@users.noreply.github.com>
2021-09-21 14:16:32 +0300

    Add files via upload

--- lab2/turtle_p2_3.py
+++ lab2/turtle_p2_3.py
@@ -32,6 +32,12 @@ def num(a,l):
     t.forward(2*l)
     t.left(90)
 
+x = int(input())
+y=[]
+while x!=0:
+    y.append(x%10)
+    x = x//10
+
 t.shape('turtle')
 t.speed(10)
 
@@ -39,9 +45,10 @@ input = open('shrift.txt', 'r')
 a = []
 for i in range(10):
     a.append(list(map(int,input.readline().split(','))))
+
 t.penup()
 t.goto(-300,0)
-for i in a[1],a[4],a[1],a[7],a[0],a[0]:
-    num(i,50)
+for j in y[::-1]:
+    num(a[j],50)
     t.forward(50)
 input.close()
