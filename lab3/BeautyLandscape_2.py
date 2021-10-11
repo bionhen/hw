d2dc4358627e6b88c2789c8eb61ca62d9de533ac
Bionhen <liubimov.is@phystech.edu>
2021-10-11 17:22:02 +0300

    Final

--- lab3/BeautyLandscape_2.py
+++ lab3/BeautyLandscape_2.py
@@ -49,42 +49,85 @@ def bush(col_fl_int, col_fl_d, col_fl_ctr, col_bsh):
     for i in range(3):
         sur.blit(pygame.transform.rotate(
             pygame.transform.scale(
-                flower(col_fl_int, col_fl_d, col_fl_ctr), (200 + 30 * i, 200 + 30 * i)), -200 + i * 200),
-            (30, 30))
+                flower(col_fl_int, col_fl_d, col_fl_ctr), (200 + 50 * i, 200 + 50 * i)), -30 + i * 30),
+            (60, 60))
 
     for i in range(3):
         sur.blit(pygame.transform.rotate(
             pygame.transform.scale(
-                flower(col_fl_int, col_fl_d, col_fl_ctr), (200 + 30 * i, 300 + 30 * i)), -200 + 30 * i),
-            (10, 30))
+                flower(col_fl_int, col_fl_d, col_fl_ctr), (200 + 50 * i, 200 + 50 * i)), -20 + 40 * i),
+            (200, 60))
     return sur
 
 
-def leg():
+# Задает ногу цветом col_leg
+# col_leg - цвет ноги
+
+
+def leg(col_leg):
     sur = pygame.Surface((500, 500), pygame.SRCALPHA)
-    ellipse(sur, WHITE, (2, 0, 50, 150))
-    ellipse(sur, WHITE, (0, 130, 55, 140))
-    ellipse(sur, WHITE, (20, 265, 50, 40))
+    ellipse(sur, col_leg, (2, 0, 50, 150))
+    ellipse(sur, col_leg, (0, 130, 55, 140))
+    ellipse(sur, col_leg, (20, 265, 50, 40))
     return sur
 
+# создает surface с животным
+# col_not - цвет животного
+# col_not_eye - цвет глаз
 
-def notunicorn(col_not):  # создаёт surface с НХЖ(неопознанное ходячее животное)
+
+def notunicorn(col_not, col_not_eye):
     sur = pygame.Surface((500, 500), pygame.SRCALPHA)
     ellipse(sur, col_not, (200, 50, 80, 50))
     ellipse(sur, col_not, (200, 80, 50, 150))
     ellipse(sur, col_not, (0, 200, 260, 120))
-    sur.blit(pygame.transform.scale(leg(), (300, 300)), (20, 280))
-    sur.blit(pygame.transform.scale(leg(), (300, 300)), (60, 300))
-    sur.blit(pygame.transform.scale(leg(), (300, 300)), (170, 270))
-    sur.blit(pygame.transform.scale(leg(), (300, 300)), (210, 290))
+    sur.blit(pygame.transform.scale(leg(col_not), (300, 300)), (20, 280))
+    sur.blit(pygame.transform.scale(leg(col_not), (300, 300)), (60, 300))
+    sur.blit(pygame.transform.scale(leg(col_not), (300, 300)), (170, 270))
+    sur.blit(pygame.transform.scale(leg(col_not), (300, 300)), (210, 290))
     polygon(sur, col_not, [(200, 30), (210, 55), (220, 59), (210, 59), (205, 60)])
     polygon(sur, col_not, [(220, 30), (230, 52), (240, 55), (230, 55), (225, 53)])
-    circle(sur, PURPLE, (240, 70), 16)
+    circle(sur, col_not_eye, (240, 70), 16)
     circle(sur, BLACK, (247, 70), 7)
     sub = pygame.Surface((15, 5), pygame.SRCALPHA)
     ellipse(sub, col_not, (0, 0, 15, 5))
     sur.blit(pygame.transform.rotate(sub, -30), (230, 58))
     return sur
+
+
+# Создание кустов:
+# Задает функцию рисования куста с параметрами:
+# сol_fl_int - цвет внутренней части
+# col_fl_d - цвет границы цветка
+# col_fl_ctr - цвет центра цветка
+# col_bsh - цвет куста
+# Растягивает картинку куста до параметров:
+# x - длина
+# y - ширина
+# Размещает левый верхний угол объекта surface в точку с координатами (z, w)
+
+
+def cr_bush(col_fl_int, col_fl_d, col_fl_ctr, col_bsh, x, y, z, w):
+    sc.blit(pygame.transform.scale(bush(col_fl_int, col_fl_d, col_fl_ctr, col_bsh), (x, y)), (z, w))
+
+
+# Создает объект surface с нарисованным животным, с параметрами:
+# col_not - цвет животного
+# col_not_eye - цвет глаз
+# x - длина
+# y - ширина
+# Размещает левый верхний угол объекта surface в точку с координатами (z, w)
+# Параметр factor отвечает за отражение
+# Если factor > 0, картинка не отражена, если < 0 отражена
+
+
+def cr_not(col_not, col_not_eye, x, y, z, w, factor):
+    if factor > 0:
+        sc.blit(pygame.transform.scale(notunicorn(col_not, col_not_eye), (x, y)), (z,w))
+    else:
+        if factor < 0:
+           sc.blit(pygame.transform.scale(pygame.transform.flip(notunicorn(col_not, col_not_eye), True, False), (x, y)), (z, w))
+
 
 
 polygon(sc, GREY,
@@ -95,28 +138,19 @@ polygon(sc, BLACK,
          (60, 100)], width=2)
 polygon(sc, GREEN, [(0, 580), (0, 1000), (800, 1000), (800, 650), (500, 650), (500, 550), (150, 550)])
 polygon(sc, BLACK, [(0, 580), (0, 1000), (800, 1000), (800, 650), (500, 650), (500, 550), (150, 550)], width=1)
-
-
-#Создание кустов
-
-
-def cr_bush(col_fl_int, col_fl_d, col_fl_ctr, col_bsh, x, y, z, w):
-    sc.blit(pygame.transform.scale(bush(col_fl_int, col_fl_d, col_fl_ctr, col_bsh), (x, y)), (z, w))
-
-
 cr_bush(WHITE, BLACK, YELLOW, DARK_GREEN, 100, 100, -10, 570)
 cr_bush(WHITE, BLACK, YELLOW, DARK_GREEN, 300, 300, 530, 750)
 cr_bush(WHITE, BLACK, YELLOW, DARK_GREEN, 200, 200, 650, 500)
-sc.blit(pygame.transform.scale(bush(WHITE, BLACK, YELLOW, DARK_GREEN), (50, 50)), (430, 530))
-sc.blit(pygame.transform.scale(bush(WHITE, BLACK, YELLOW, DARK_GREEN), (75, 75)), (330, 530))
-sc.blit(pygame.transform.scale(bush(WHITE, BLACK, YELLOW, DARK_GREEN), (80, 80)), (500, 600))
+cr_bush(WHITE, BLACK, YELLOW, DARK_GREEN, 50, 50, 430, 530)
+cr_bush(WHITE, BLACK, YELLOW, DARK_GREEN, 75, 75, 330, 530)
+cr_bush(WHITE, BLACK, YELLOW, DARK_GREEN, 80, 80, 500, 600)
 
-sc.blit(pygame.transform.scale(notunicorn(WHITE), (700, 700)), (-270, 760))
-sc.blit(pygame.transform.scale(notunicorn(WHITE), (150, 150)), (270, 500))
-sc.blit(pygame.transform.scale(notunicorn(WHITE), (150, 150)), (190, 600))
+cr_not(WHITE, PURPLE, 700, 700, -270, 760, 1)
+cr_not(WHITE, PURPLE, 150, 150, 270, 500, 1)
+cr_not(WHITE, PURPLE, 150, 150, 190, 600, 1)
 
-sc.blit(pygame.transform.scale(pygame.transform.flip(notunicorn(WHITE), True, False), (100, 100)), (350, 600))
-sc.blit(pygame.transform.scale(pygame.transform.flip(notunicorn(WHITE), True, False), (500, 500)), (450, 600))
+cr_not(WHITE, PURPLE, 100, 100, 350, 600, -1)
+cr_not(WHITE, PURPLE, 500, 500, 450, 600, -1)
 
 pygame.display.update()
 clock = pygame.time.Clock()
